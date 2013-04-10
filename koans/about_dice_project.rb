87eb01ec38acf6aca4a84ccb4e21c7f37bf5571a
Sid Jayanna <sjayanna@gmail.com>
2013-04-10 00:14:54 -0400

    finish dice project koan

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+ class DiceSet
+ attr_accessor	:values
+
+ 	def roll(n)
+		@values = []
+		n.times do
+			@values << 1 + rand(6)
+		end	
+	end
+ 	
+ end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
